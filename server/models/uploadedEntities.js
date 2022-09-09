aa5d2abb791755a664dbd02fc72fc864055f2b1f
ergi <ergi@student.chalmers.se>
2022-09-09 16:51:32 +0200

    Improved the formatting

--- server/models/uploadedEntities.js
+++ server/models/uploadedEntities.js
@@ -2,24 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const uploadedEntitiesSchema = new Schema({
-        file:{
-            path: String,
+    file:{
+        path: String,
+        required: true
+    },
+    metadata:[{
+        filename:{
+            type: String,
             required: true
         },
-        metadata:[{
-            filename:{
-                type: String,
-                required: true
-            },
-            location:{
-                type:String,
-                required: false
-            }
+        location:{
+            type:String,
+            required: false
         }
-
-
-        ]
-    }
-
-)
+    }]
+});
 
