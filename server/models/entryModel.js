192e6f77d68d16741f9dba31ae4a9d879fce207e
ergi <ergi@student.chalmers.se>
2022-09-12 15:48:46 +0200

    Implemented UUID and POST method
    Co-authored-by askan:<askan@student.chalmers.se>

--- server/models/entryModel.js
+++ server/models/entryModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const entrySchema = new Schema({
-    ID: {
+    _id: {
         type: String,
         required: true
     },
