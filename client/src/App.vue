6095259cf48f4c1063603b874dc7cc64c6c0c7af
Mathias Hallander <mathalla@student.chalmers.se>
2022-09-29 15:23:07 +0200

    Auto stash before merge of "Login-and-Registration-Frontend" and "origin/Login-and-Registration-Frontend"

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div class="container" id="app">
     <div id="nav">
       <router-link to="/"></router-link>
     </div>
--- client/src/views/Home-reg.vue
+++ client/src/views/Home-reg.vue
@@ -1,51 +1,50 @@
 <template>
   <body>
-      <div class="container" id = "topContainer">
-      <div class="row">
-          <div class="container col-md-4" id="parentContainer">
-            <form id = "registrationForm">
-              <h2 class="text-center mb-5">Create an account</h2>
-              <div class="form-floating mb-4">
-                <input type="text" class="form-control form-control-lg" id="fName" placeholder="a" required>
-                <label>First Name</label>
-              </div>
-
-              <div class="form-floating mb-4">
-                <input type="text" class="form-control form-control-lg" id="lName" placeholder="b" required>
-                <label>Last Name</label>
-              </div>
-
-              <div class="form-floating mb-4">
-                <input type="email" class="form-control form-control-lg" id="email" placeholder="deeznuts@mail.com" required>
-                <label>Email</label>
-              </div>
-
-              <div class="form-floating mb-4">
-                <input type="password" class="form-control form-control-lg" id="pass" placeholder="b" required>
-                <label>Password</label>
-              </div>
-
-              <div class="form-floating mb-4">
-                <input type="password" class="form-control form-control-lg" id="confPass" placeholder="b" required>
-                <label>Confirm Password</label>
-              </div>
-
-                  <div class="form-check d-flex mb-2">
-                    <input class="form-check-input me-2" type="checkbox" value="" id="tosCheckbox" />
-                    <label class="form-check-label">I accept the <a href="#!" class="text-body"><u>Terms of Service</u></a>
-                    </label>
-                  </div>
-
-                  <button type="submit" class="btn btn-primary" style="width: 150px; height: 50px">Sing Up</button>
-
-                  <p class="text-center text-muted mt-3 mb-2">Already have an account? <a href="#!"
-                  class="fw-bold text-body"><u>Login here</u></a></p>
-
-                </form>
+    <div class="row" id="rowContainer">
+      <div class="col-md-4" id="parentContainer">
+        <form id="registrationForm">
+          <h2 class="text-center mb-5">Create an account</h2>
+          <div class="form-floating mb-4">
+            <input type="text" class="form-control form-control-lg" id="fName" placeholder="a" required>
+            <label>First Name</label>
           </div>
-        </div>
+
+          <div class="form-floating mb-4">
+            <input type="text" class="form-control form-control-lg" id="lName" placeholder="b" required>
+            <label>Last Name</label>
+          </div>
+
+          <div class="form-floating mb-4">
+            <input type="email" class="form-control form-control-lg" id="email" placeholder="deeznuts@mail.com"
+                   required>
+            <label>Email</label>
+          </div>
+
+          <div class="form-floating mb-4">
+            <input type="password" class="form-control form-control-lg" id="pass" placeholder="b" required>
+            <label>Password</label>
+          </div>
+
+          <div class="form-floating mb-4">
+            <input type="password" class="form-control form-control-lg" id="confPass" placeholder="b" required>
+            <label>Confirm Password</label>
+          </div>
+
+          <div class="form-check d-flex mb-2">
+            <input class="form-check-input me-2" type="checkbox" value="" id="tosCheckbox"/>
+            <label class="form-check-label">I accept the <a href="#!" class="text-body"><u>Terms of Service</u></a>
+            </label>
+          </div>
+
+          <button type="submit" class="btn btn-primary" style="width: 150px; height: 50px">Sing Up</button>
+
+          <p class="text-center text-muted mt-3 mb-2">Already have an account?
+            <a href="#!" class="fw-bold text-body"> <u>Login here</u></a>
+          </p>
+        </form>
       </div>
-</body>
+    </div>
+  </body>
 </template>
 
 <script>
@@ -73,19 +72,34 @@
 
 <style>
 
-#registrationForm{
+#registrationForm {
   vertical-align: auto;
 }
 #topContainer{
   padding: 20px;
 }
-Html, body{
-  height:100vh;
+#rowContainer {
+  height: 100%;
+  padding: 10px;
+  margin: 10px;
 }
-#parentContainer{
-  display:table-cell; vertical-align:middle;
-  background-color:greenyellow;
+
+#registrationForm {
+  width: auto;
+  vertical-align: auto;
+}
+
+Html, body {
+  height: 100vh;
+  align-items: center;
+}
+
+#parentContainer {
+  vertical-align: middle;
+  display: table-cell;
+  background-color: greenyellow;
   width: 600px;
+  padding: 50px;
   height: 700px;
   margin: auto;
   padding: 50px;
