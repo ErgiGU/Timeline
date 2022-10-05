bd6b413206a6294476b98710f790a5b7ffa402ed
ergi <ergi@student.chalmers.se>
2022-10-05 02:22:52 +0200

    Implemented regular expressions
    #2 Implemented regular expressions for each field and a regular expression for the confirm password field that ensures that the passwords match

--- client/src/views/Home-reg.vue
+++ client/src/views/Home-reg.vue
@@ -4,6 +4,7 @@
       <div class="col-md-4" id="parentContainer">
         <form id="registrationForm">
           <h2 class="text-center mb-5">Create an account</h2>
+
           <div class="form-floating mb-4">
             <input type="text" class="form-control form-control-lg" id="fName" placeholder="a" required>
             <label>First Name</label>
@@ -16,17 +17,20 @@
 
           <div class="form-floating mb-4">
             <input type="email" class="form-control form-control-lg" id="email" placeholder="deeznuts@mail.com"
-                   required>
+                   required pattern="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$" title="Please enter a valid email.">
             <label>Email</label>
           </div>
 
           <div class="form-floating mb-4">
-            <input type="password" class="form-control form-control-lg" id="pass" placeholder="b" required>
+            <input type="password" class="form-control form-control-lg" id="pass"  title="
+            Password must contain: Minimum 8 characters at least 1 alphabetic character and 1 number"
+                   placeholder="b" required=""
+                   pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$">
             <label>Password</label>
           </div>
 
           <div class="form-floating mb-4">
-            <input type="password" class="form-control form-control-lg" id="confPass" placeholder="b" required>
+            <input type="password" class="form-control form-control-lg" id="confPass"  placeholder="b" required>
             <label>Confirm Password</label>
           </div>
 
@@ -36,7 +40,7 @@
             </label>
           </div>
 
-          <button type="submit" class="btn btn-primary" style="width: 150px; height: 50px">Sing Up</button>
+          <button class="btn submits sign-up" id="btn1" @click="function1" style="width: 150px; height: 50px">Sign Up</button>
 
           <p class="text-center mt-3 mb-2">Already have an account?
             <router-link to="/" style="color: black">Login here</router-link>
@@ -48,26 +52,35 @@
 </template>
 
 <script>
+
 // @ is an alias to /src
-(function () {
-  'use strict'
+export default {
+  name: "App",
+  data() {
+    return {}
+  },
+  methods: {
+    function1() {
+      const fName = document.getElementById('fName');
+      const lName = document.getElementById('lName');
+      const email = document.getElementById('email');
+      const pass = document.getElementById('pass');
+      const confPass = document.getElementById('confPass');
+      if(pass.value !== confPass.value) {
+        confPass.setCustomValidity("Passwords don't match");
+      } else {
+        confPass.setCustomValidity('');
+      }
+      console.log(fName.value);
+      console.log(lName.value);
+      console.log(email.value);
+      console.log(pass.value);
+      console.log(confPass.value);
+    },
+  },
+};
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  let forms = document.querySelectorAll('.needs-validation')
 
-  // Loop over them and prevent submission
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-
-        form.classList.add('was-validated')
-      }, false)
-    })
-})()
 </script>
 
 <style>
@@ -91,12 +104,18 @@ Html, body {
 #parentContainer {
   vertical-align: middle;
   display: table-cell;
-  background-color: greenyellow;
   width: 600px;
   padding: 50px;
   height: 700px;
   margin: auto;
-  border-radius: 14px;
+  background-color: rgba(255,255,255,0.13);
+  border-radius: 10px;
+  backdrop-filter: blur(10px);
+  border: 2px solid rgba(255,255,255,0.1);
+  box-shadow: 0 0 40px rgba(8,7,16,0.6);
+}
+#btn1{
+  background-color: cornflowerblue;
 }
 
 </style>
