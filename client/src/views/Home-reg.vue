116d051d4fa7a14a8b8bb846e26c3e384992e985
ergi <ergi@student.chalmers.se>
2022-10-05 17:46:40 +0200

    Functional checkbox and regexes for all the fields
    Made the regexes functional so that it prevents the user from registering if the the input from the fields is invalid.

--- client/src/views/Home-reg.vue
+++ client/src/views/Home-reg.vue
@@ -1,54 +1,54 @@
 <template>
-  <body>
-    <div class="row" id="rowContainer">
-      <div class="col-md-4" id="parentContainer">
-        <form id="registrationForm">
-          <h2 class="text-center mb-5">Create an account</h2>
+  <div class="row needs-validation"  id="rowContainer" >
+    <div class="col-md-4" id="parentContainer">
+      <form id="registrationForm" v-on:submit="function1">
+        <h2 class="text-center mb-5 text-white">Create an account</h2>
 
-          <div class="form-floating mb-4">
-            <input type="text" class="form-control form-control-lg" id="fName" placeholder="a" required>
-            <label>First Name</label>
-          </div>
+        <div class="form-floating mb-4">
+          <input type="text" class="form-control form-control-lg" id="fName" placeholder="a" required>
+          <label>First Name</label>
+          <div class="valid-feedback text-danger">Looks good!</div>
+        </div>
 
-          <div class="form-floating mb-4">
-            <input type="text" class="form-control form-control-lg" id="lName" placeholder="b" required>
-            <label>Last Name</label>
-          </div>
+        <div class="form-floating mb-4">
+          <input type="text" class="form-control form-control-lg" id="lName" placeholder="b" required>
+          <label>Last Name</label>
+        </div>
 
-          <div class="form-floating mb-4">
-            <input type="email" class="form-control form-control-lg" id="email" placeholder="deeznuts@mail.com"
-                   required pattern="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$" title="Please enter a valid email.">
-            <label>Email</label>
-          </div>
+        <div class="form-floating mb-4">
+          <input type="email" class="form-control form-control-lg" id="email" placeholder="exampleEmail"
+                 required pattern="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$" title="Please enter a valid email.">
+          <label>Email</label>
+        </div>
 
-          <div class="form-floating mb-4">
-            <input type="password" class="form-control form-control-lg" id="pass"  title="
-            Password must contain: Minimum 8 characters at least 1 alphabetic character and 1 number"
-                   placeholder="b" required=""
-                   pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$">
-            <label>Password</label>
-          </div>
+        <div class="form-floating mb-4">
+          <input type="password" class="form-control form-control-lg" id="pass"  title="
+          Password must contain: Minimum 8 characters at least 1 alphabetic character and 1 number"
+                 placeholder="b" required=""
+                 pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$">
+          <label>Password</label>
+        </div>
 
-          <div class="form-floating mb-4">
-            <input type="password" class="form-control form-control-lg" id="confPass"  placeholder="b" required>
-            <label>Confirm Password</label>
-          </div>
+        <div class="form-floating mb-4">
+          <input type="password" class="form-control form-control-lg" id="confPass" v-on:keyup="checkIfPassMatches"
+                 placeholder="b" required>
+          <label>Confirm Password</label>
+        </div>
 
-          <div class="form-check d-flex mb-2">
-            <input class="form-check-input me-2" type="checkbox" value="" id="tosCheckbox"/>
-            <label class="form-check-label">I accept the <a href="#!" class="text-body"><u>Terms of Service</u></a>
-            </label>
-          </div>
+        <div class="form-check d-flex mb-2">
+          <input class="form-check-input me-2" type="checkbox" v-on:change="checkBox()" value="" id="tosCheckbox"/>
+          <label class="form-check-label text-white">I accept the <a href="#!" class="text-body "><u>Terms of Service</u></a>
+          </label>
+        </div>
 
-          <button class="btn submits sign-up" id="btn1" @click="function1" style="width: 150px; height: 50px">Sign Up</button>
-
-          <p class="text-center mt-3 mb-2">Already have an account?
-            <router-link to="/" style="color: black">Login here</router-link>
-          </p>
-        </form>
-      </div>
+        <button class="btn1 btn submits text-white sign-up disabled" id="btn1"
+                style="width: 150px; height: 50px">Sign Up</button>
+        <p class="text-center mt-3 mb-2 text-white">Already have an account?
+          <router-link to="/" style="color: black">Login here</router-link>
+        </p>
+      </form>
     </div>
-  </body>
+  </div>
 </template>
 
 <script>
@@ -57,33 +57,63 @@
 export default {
   name: "App",
   data() {
-    return {}
+    return {checked: false}
   },
   methods: {
-    function1() {
+    function1(event) {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      //const forms = document.querySelectorAll('.needs-validation');
       const fName = document.getElementById('fName');
       const lName = document.getElementById('lName');
       const email = document.getElementById('email');
       const pass = document.getElementById('pass');
       const confPass = document.getElementById('confPass');
-      if(pass.value !== confPass.value) {
+      /*    console.log(fName.checkValidity());
+      console.log(lName.checkValidity());
+      console.log(email.checkValidity());
+      console.log(pass.checkValidity());
+      console.log(confPass.checkValidity());*/
+
+      if (fName.checkValidity() && lName.checkValidity() && email.checkValidity() && pass.checkValidity() &&
+        confPass.checkValidity()) {
+        console.log(fName.value);
+        console.log(lName.value);
+        console.log(email.value);
+        console.log(pass.value);
+        console.log(confPass.value);
+
+      } else {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+    },
+    checkBox() {
+      const okButton = document.getElementById('btn1')
+      if (!this.checked) {
+        okButton.classList.remove('disabled')
+        this.checked = true
+      } else {
+        okButton.classList.add('disabled')
+        this.checked = false
+      }
+    },
+    checkIfPassMatches() {
+      const pass = document.getElementById('pass');
+      const confPass = document.getElementById('confPass');
+      if (pass.value !== confPass.value) {
         confPass.setCustomValidity("Passwords don't match");
       } else {
         confPass.setCustomValidity('');
       }
-      console.log(fName.value);
-      console.log(lName.value);
-      console.log(email.value);
-      console.log(pass.value);
-      console.log(confPass.value);
-    },
-  },
-};
+    }
+  }
+}
 
 
 </script>
 
 <style>
+
 
 #rowContainer {
   height: 100%;
@@ -96,9 +126,10 @@ export default {
   vertical-align: auto;
 }
 
-Html, body {
+body {
   height: 100vh;
   align-items: center;
+  background: #3b3b3b;
 }
 
 #parentContainer {
